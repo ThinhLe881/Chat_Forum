1af63644570d54de5e4f10e39a49a0203a5d569a
ThinhLe881 <thinhleminh201@gmail.com>
2023-01-08 00:18:50 -0500

    fix header

---
+++ client/src/components/Avatar.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+type Props = {
+	seed?: string;
+	small?: boolean;
+	large?: boolean;
+};
+
+function Avatar({ seed, small, large }: Props) {
+	const userId = '63af5463218aecbee5bde0bc';
+	return (
+		<div
+			className={`relative h-10 w-10 overflow-hidden rounded-full border-gray-300 bg-white ${
+				small && 'h-5 w-5 flex-shrink-0'
+			} ${large && 'h-20 w-20'}`}
+		>
+			<img
+				src={`https://avatars.dicebear.com/api/open-peeps/${userId || 'placeholder'}.svg`}
+			/>
+		</div>
+	);
+}
+
+export default Avatar;
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -11,29 +11,27 @@ import {
 } from '@heroicons/react/24/outline';
 import Logo1 from '../assets/logo-1.png';
 import Logo2 from '../assets/logo-2.png';
+import Avatar from './Avatar';
 
 function Header() {
 	return (
-		<div className='sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm'>
-			<div
-				className='h-15 relative flex w-20 flex-shrink-0 cursor-pointer 
-			items-center'
-			>
+		<div className='sticky top-0 z-50 flex justify-evenly bg-white py-2 shadow-sm md:px-4'>
+			<div className='h-15 relative flex w-20 flex-shrink-0 cursor-pointer items-center'>
 				<img src={Logo1} />
 			</div>
 
 			<div
-				className='mx-6 flex cursor-pointer items-center rounded-sm p-2
-			 hover:bg-gray-100 xl:min-w-[300px]'
+				className='flex cursor-pointer items-center rounded-sm p-2
+			 hover:bg-gray-100 md:mx-6 xl:min-w-[300px]'
 			>
 				<HomeIcon className='h-5 w-5' />
 				<p className='mx-2 hidden flex-1 lg:inline'>Home</p>
-				<ChevronDownIcon className='h-5 w-5' />
+				<ChevronDownIcon className='hidden md:block md:h-5 md:w-5' />
 			</div>
 
 			<form
-				className='flex flex-1 cursor-pointer items-center space-x-2
-       rounded-full border-gray-200 bg-gray-100 px-3 py-1 hover:ring-1'
+				className='flex max-w-[200px] flex-1 cursor-pointer items-center space-x-2
+       rounded-full border-gray-200 bg-gray-100 px-3 py-1 hover:ring-1 md:max-w-none'
 			>
 				<MagnifyingGlassIcon className='h-6 w-6 text-gray-400' />
 				<input
@@ -49,7 +47,7 @@ function Header() {
 
 			<div
 				className='mx-5 hidden items-center space-x-2 text-gray-600 
-			lg:inline-flex'
+			md:inline-flex'
 			>
 				<ArrowTrendingUpIcon className='icon' />
 				<CircleStackIcon className='icon' />
@@ -61,10 +59,6 @@ function Header() {
 				<PlusIcon className='icon' />
 			</div>
 
-			<div className='ml-5 flex items-center lg:hidden'>
-				<Bars3Icon className='icon' />
-			</div>
-
 			<div
 				className='hidden cursor-pointer items-center space-x-2 
 			border border-gray-100 p-2 lg:flex'
@@ -73,6 +67,10 @@ function Header() {
 					<img src={Logo2} />
 				</div>
 				<p>Sign In</p>
+			</div>
+
+			<div className='flex items-center lg:hidden'>
+				<Bars3Icon className='icon' />
 			</div>
 		</div>
 	);
