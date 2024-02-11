2be2973919d6ef808cce723622328bd73af5bb3a
ThinhLe881 <thinhleminh201@gmail.com>
2024-02-11 17:17:04 -0500

    finish user status on navbar

--- client/src/components/Avatar/UserAvatar.tsx
+++ client/src/components/Avatar/UserAvatar.tsx
@@ -4,10 +4,8 @@ type Props = {
 
 const UserAvatar = ({ userId }: Props) => {
 	return (
-		<div
-			className={`'h-7 w-7' relative h-10 w-10 flex-shrink-0 overflow-hidden rounded-full bg-white`}
-		>
-			<img src={`https://avatars.dicebear.com/api/open-peeps/${userId}.svg`} />
+		<div className={`relative h-9 w-9 flex-shrink-0 overflow-hidden rounded-full bg-white`}>
+			<img src={`https://robohash.org/${userId}`} />
 		</div>
 	);
 };
--- client/src/components/Header/UserStatus.tsx
+++ client/src/components/Header/UserStatus.tsx
@@ -1,9 +1,32 @@
+import { ChevronDownIcon, StarIcon } from '@heroicons/react/24/solid';
 import { useAuth } from '../../utils/AuthContext';
+import UserAvatar from '../Avatar/UserAvatar';
+import Spinner from '../Utils/Spinner';
 
 const UserStatus = () => {
 	const { user } = useAuth();
 
-	return <div>{user ? user.name : ''}</div>;
+	return (
+		<div className='flex min-h-10 min-w-52 items-center justify-between rounded-md px-1 hover:border'>
+			{user ? (
+				<div className='flex flex-grow gap-x-1 '>
+					<UserAvatar userId={user.id} />
+					<div className='flex flex-grow flex-col'>
+						<div className='text-sm font-medium'>{user.name}</div>
+						<div className='flex items-center gap-x-0.5'>
+							<StarIcon className='h-3 w-3 text-orange-600' />
+							<div className='text-xs text-slate-500'>{user.votes} votes</div>
+						</div>
+					</div>
+				</div>
+			) : (
+				<div className='flex flex-grow items-center justify-center'>
+					<Spinner />
+				</div>
+			)}
+			<ChevronDownIcon className='h-4 w-4 text-slate-500' />
+		</div>
+	);
 };
 
 export default UserStatus;
